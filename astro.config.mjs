d21f05772069ee934ed2b06b80fb90d1727d204c
Jaylaelike <smarkwisai@gmail.com>
2024-05-29 14:14:55 +0700

    Config add vercel Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,41 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import { remarkReadingTime } from './src/utils/readTime.ts';
-import embeds from 'astro-embed/integration';
-import vercel from '@astrojs/vercel/serverless';
-
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import { remarkReadingTime } from './src/utils/readTime.ts'
+import embeds from 'astro-embed/integration'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://engineering-blog-thaipbs.vercel.app/',
-  // Write here your website url
-  markdown: {
-    remarkPlugins: [remarkReadingTime],
-    drafts: true,
-    shikiConfig: {
-      theme: 'material-theme-palenight',
-      wrap: true
-    }
-  },
-  integrations: [embeds(), mdx({
-    syntaxHighlight: 'shiki',
-    shikiConfig: {
-      experimentalThemes: {
-        light: 'vitesse-light',
-        dark: 'material-theme-palenight'
-      },
-      wrap: true
-    },
-    drafts: true
-  }), sitemap(), tailwind()],
-  output: 'server',
-  adapter: vercel()
-});
+	site: 'https://engineering-blog-thaipbs.vercel.app/',
+	// Write here your website url
+	markdown: {
+		remarkPlugins: [remarkReadingTime],
+		drafts: true,
+		shikiConfig: {
+			theme: 'material-theme-palenight',
+			wrap: true
+		}
+	},
+	integrations: [
+		embeds(),
+		mdx({
+			syntaxHighlight: 'shiki',
+			shikiConfig: {
+				experimentalThemes: {
+					light: 'vitesse-light',
+					dark: 'material-theme-palenight'
+				},
+				wrap: true
+			},
+			drafts: true
+		}),
+		sitemap(),
+		tailwind()
+	],
+	output: 'server',
+	adapter: vercel({
+		webAnalytics: { enabled: true }
+	})
+})
